c70ccd8122b9b71b6ef4a34c12fc38b189bd6cd7
gudofabi <godofredocpanel@gmail.com>
2024-10-14 19:17:33 +0800

    fix: update main

---
+++ components/Test/Palindrome.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>Palindrome</h2>
+  <input v-model="inputText" placeholder="Enter word" />
+  <button @click="func_clickButton">Submit</button>
+  <p v-if="result != ''">{{ result }}</p>
+</template>
+<script setup lang="ts">
+const inputText = ref("");
+const result = ref("");
+
+const func_clickButton = () => {
+  const cleanedText = inputText.value
+    .replace(/[^A-Za-z0-9]/g, "")
+    .toLowerCase();
+  const reversedText = cleanedText.split("").reverse().join("");
+
+  console.log(cleanedText);
+  console.log(reversedText);
+  if (cleanedText === reversedText) {
+    result.value = "This is a palidrome";
+  } else {
+    result.value = "Are you kidding me?";
+  }
+};
+</script>
---
+++ components/Test/ReadNumber.vue
@@ -0,0 +1,34 @@
+<template>
+  <h2>Read the Number Component</h2>
+  <input v-model="inputNumber" placeholder="Enter a number between 1 - 500" />
+  <button @click="func_clickButton">Submit</button>
+  <ul v-if="results.length">
+    <li v-for="(result, index) in results" :key="index">{{ result }}</li>
+  </ul>
+</template>
+<script setup lang="ts">
+const inputNumber = ref(null);
+const results = ref([]);
+
+const func_clickButton = () => {
+  const num = parseInt(inputNumber.value);
+
+  if (num >= 1 && num <= 500) {
+    results.value = [];
+
+    for (let i = 1; i <= num; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        results.value.push("Benchmarking");
+      } else if (i % 3 === 0) {
+        results.value.push("Bench");
+      } else if (i % 5 === 0) {
+        results.value.push("Marking");
+      } else {
+        results.value.push(i);
+      }
+    }
+  } else {
+    alert("Please enter a number between 1 to 500");
+  }
+};
+</script>
